b5d2a66106152e5b0caeef07faf32a96c2df5c2b
cabamarcos <marcos.caballero.0203@gmail.com>
2022-11-16 12:51:35 +0100

    rsa

---
+++ rsa.py
@@ -0,0 +1,14 @@
+#import rsa
+from Crypto.PublicKey import RSA
+#Create a function to generate a public/private key pair
+def generate_keys():
+    #Generate a public/private key pair using 4096 bits key length (512 bytes)
+    new_key = RSA.generate(4096, e=65537)
+    #The private key in PEM format
+    private_key = new_key.exportKey("PEM")
+    #The public key in PEM Format
+    public_key = new_key.publickey().exportKey("PEM")
+    #Return both the private and the public key
+    return private_key, public_key
+
+print(generate_keys())
